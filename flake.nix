a31e01e5f7b26f14331672eb8f33723ad4796e6e
Kenichi Kamiya <kachick1@gmail.com>
2024-05-16 20:00:02 +0900

    Inline systems in flake schema and drop flake-utils
    Import https://github.com/kachick/anylang-template/pull/98

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
     #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     edge-nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
@@ -15,53 +14,77 @@
       self,
       nixpkgs,
       edge-nixpkgs,
-      flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        edge-pkgs = edge-nixpkgs.legacyPackages.${system};
-      in
-      rec {
-        formatter = edge-pkgs.nixfmt-rfc-style;
-        devShells.default =
-          with pkgs;
-          mkShell {
-            buildInputs = [
-              # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
-              bashInteractive
-              nil
-              edge-pkgs.nixfmt-rfc-style
+    let
+      # Candidates: https://github.com/NixOS/nixpkgs/blob/release-23.11/lib/systems/flake-systems.nix
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        # I don't have M1+ mac, providing this for macos-14 free runner https://github.com/actions/runner-images/issues/9741
+        "aarch64-darwin"
+      ];
+    in
+    rec {
+      formatter = forAllSystems (system: edge-nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          edge-pkgs = edge-nixpkgs.legacyPackages.${system};
+        in
+        {
+          default =
+            with pkgs;
+            mkShell {
+              buildInputs = [
+                # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
+                bashInteractive
+                nil
+                edge-pkgs.nixfmt-rfc-style
 
-              edge-pkgs.go_1_22
-              edge-pkgs.dprint
-              edge-pkgs.goreleaser
-              edge-pkgs.typos
-              go-task
+                edge-pkgs.go_1_22
+                edge-pkgs.dprint
+                edge-pkgs.goreleaser
+                edge-pkgs.typos
+                go-task
+              ];
+            };
+        }
+      );
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          edge-pkgs = edge-nixpkgs.legacyPackages.${system};
+          version = "v1.1.2";
+        in
+        rec {
+          selfup = edge-pkgs.buildGo122Module {
+            pname = "selfup";
+            src = pkgs.lib.cleanSource self;
+            version = version;
+            ldflags = [
+              "-X main.version=${version}"
+              "-X main.commit=${if (self ? rev) then self.rev else "0000000000000000000000000000000000000000"}"
             ];
+
+            # When updating go.mod or go.sum, update this sha together as following
+            vendorHash = "sha256-wZb6U2CSYZvFyT7rT1FWMZpKpuAXjINZrfV0breLguI=";
           };
 
-        packages.selfup = edge-pkgs.buildGo122Module rec {
-          pname = "selfup";
-          src = pkgs.lib.cleanSource self;
-          version = "v1.1.2";
-          ldflags = [
-            "-X main.version=${version}"
-            "-X main.commit=${if (self ? rev) then self.rev else "0000000000000000000000000000000000000000"}"
-          ];
+          default = selfup;
+        }
+      );
 
-          # When updating go.mod or go.sum, update this sha together as following
-          vendorHash = "sha256-wZb6U2CSYZvFyT7rT1FWMZpKpuAXjINZrfV0breLguI=";
-        };
-
-        packages.default = packages.selfup;
-
-        # `nix run`
-        apps.default = {
+      # `nix run`
+      apps = forAllSystems (system: {
+        default = {
           type = "app";
-          program = "${packages.selfup}/bin/selfup";
+          program = "${packages.${system}.selfup}/bin/selfup";
         };
-      }
-    );
+      });
+    };
 }
