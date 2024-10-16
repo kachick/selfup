4efecbd823d9e0b3460273ac862402813707af4e
Kenichi Kamiya <kachick1@gmail.com>
2024-10-16 13:22:19 +0900

    Bump to v1.1.5

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          version = "v1.1.4";
+          version = "v1.1.5";
         in
         rec {
           selfup = pkgs.buildGo123Module {
