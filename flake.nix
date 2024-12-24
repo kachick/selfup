bc4579884a7771002c416afdcc64d390cfa932f8
Kenichi Kamiya <kachick1@gmail.com>
2024-12-24 22:55:43 +0900

    Ensure CGO_ENABLED=0

--- flake.nix
+++ flake.nix
@@ -60,6 +60,10 @@
             # vendorHash = pkgs.lib.fakeHash; # Enable this and run `nix run . -- --version`, then you can get actual hash
             vendorHash = "sha256-tUUlrUaqShYfBbKiewTppgEK1hdHp9S+3yV/hTNT7LY=";
 
+            # https://github.com/kachick/times_kachick/issues/316
+            # TODO: Use env after nixos-25.05. See https://github.com/NixOS/nixpkgs/commit/905dc8d978b38b0439905cb5cd1faf79163e1f14#diff-b07c2e878ff713081760cd5dcf0b53bb98ee59515a22e6007cc3d974e404b220R24
+            CGO_ENABLED = 0;
+
             meta.mainProgram = "selfup";
           };
 
