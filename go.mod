f13489e83b541a6229be42564a3900afc685e311
Kenichi Kamiya <kachick1@gmail.com>
2025-05-13 13:56:32 +0900

    Bump go.mod and vendorHash with adjusting to nix-update (#356)
    * Bump golang.org/x/term from 0.31.0 to 0.32.0
    
    Bumps [golang.org/x/term](https://github.com/golang/term) from 0.31.0 to 0.32.0.
    - [Commits](https://github.com/golang/term/compare/v0.31.0...v0.32.0)
    
    ---
    updated-dependencies:
    - dependency-name: golang.org/x/term
      dependency-version: 0.32.0
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Don't depend on lib.fileset.gitTracked
    
    * `nix-update selfup --version=skip --flake`
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -5,12 +5,12 @@ go 1.23.1
 require (
 	github.com/fatih/color v1.18.0
 	github.com/google/go-cmp v0.7.0
-	golang.org/x/term v0.31.0
+	golang.org/x/term v0.32.0
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2
 )
 
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
 )
--- package.nix
+++ package.nix
@@ -11,18 +11,17 @@ in
 buildGo123Module rec {
   pname = "selfup";
   version = "1.1.9";
-  src =
-    with lib.fileset;
-    toSource rec {
-      root = ./.;
-      # Don't just use `fileset.gitTracked root`, then always rebuild even if just changed the README.md
-      fileset = intersection (gitTracked root) (unions [
-        ./go.mod
-        ./go.sum
-        ./cmd
-        ./internal
-      ]);
-    };
+  src = lib.fileset.toSource {
+    root = ./.;
+    # - Don't just use `fileset.gitTracked root`, then always rebuild even if just changed the README.md
+    # - Don't use gitTracked for now, even if filtering with intersection, the feature is not supported in nix-update. See https://github.com/Mic92/nix-update/issues/335
+    fileset = lib.fileset.unions [
+      ./go.mod
+      ./go.sum
+      ./cmd
+      ./internal
+    ];
+  };
   # src = lib.cleanSource self; # Requires this old style if I use nix-update
   ldflags = [
     "-X main.version=v${version}"
@@ -30,7 +29,7 @@ buildGo123Module rec {
   ];
 
   # When updating go.mod or go.sum, update this sha together with `nix-update selfup --version=skip --flake`
-  vendorHash = "sha256-HkViZe6DfFOHe6j2R03pH5FV0Y6YXhbGPOraTnTsa6g=";
+  vendorHash = "sha256-rLS2bLpPM0Uo/fhLXTwBTimO0r8Y3IvYvMa3mK36DyQ=";
 
   # https://github.com/kachick/times_kachick/issues/316
   # TODO: Use env after nixos-25.05. See https://github.com/NixOS/nixpkgs/commit/905dc8d978b38b0439905cb5cd1faf79163e1f14#diff-b07c2e878ff713081760cd5dcf0b53bb98ee59515a22e6007cc3d974e404b220R24
